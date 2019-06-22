6b1777989311ff9cdaa224b16b19b2699834c0f6
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-06-21 19:19:29 -0700

    added url specfic functionality

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var foo = require("http");
+var url = require("url");
 
 function start() {
     function onRequest(request, response) { 
+        var pathname = url.parse(request.url).pathname;
         console.log("Request received");
         response.writeHead(200, {"Content-Type": "text/plain"}); 
         response.write("Hello world");
@@ -9,7 +11,6 @@ function start() {
     }
     
     foo.createServer(onRequest).listen(8888);
-    
     console.log("server has started");
 }
 
