b917e5e20db990cc4dbd4baccc803005ecc1236f
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-06-21 18:43:53 -0700

    database.query with var

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 var http = require("http");
+database.query("SELECT * from hugetable", functions(rows) {
+    var: result = rows
+});
+
+console.log("hello world");
 
 function onRequest(request, response) { 
     response.writeHead(200, {"Content-Type": "text/plain"}); 
