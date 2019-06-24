c1ecda530b6841292bf8948010f775c58ad38d74
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-06-24 07:51:36 -0700

    10sec timeout on start but no timeout on upload

--- requestHandlers.js
+++ requestHandlers.js
@@ -3,7 +3,9 @@ var exec = require("child_process").exec;
 function start() {
     console.log("Request handler 'start' was called");
 
-    exec("ls - lah", function (error, stdout, stderr) {
+    exec("find /", 
+        { timeout: 10000, macBuffer: 20000*1024 },
+        function (error, stdout, stderr) {
         response.writeHead(200, {"Content-Type": "text/plain"});
         response.write(stdout);
         response.end();
