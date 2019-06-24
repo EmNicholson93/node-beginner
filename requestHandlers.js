90e011aacfc6746df89318f511b2bb2e57978c4a
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-06-24 08:02:28 -0700

    start handler now adds form

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,15 +1,24 @@
 var exec = require("child_process").exec;
 
-function start() {
+function start(response) {
     console.log("Request handler 'start' was called");
 
-    exec("find /", 
-        { timeout: 10000, macBuffer: 20000*1024 },
-        function (error, stdout, stderr) {
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write(stdout);
+    var body = `
+        <html>
+            <head>
+                <meta http-equiv="Content-Type" content="text/html charset=UTF-8">
+            </head>
+            <body>
+                <form action="/upload" method="post">
+                    <textarea name="text" rows="20" cols="60"></textarea>
+                    <input type="submit" value="Submit Text">
+                </form>
+            </body>
+        </html>
+        `;
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write(body);
         response.end();
-    });
 }
 
 function upload() {
